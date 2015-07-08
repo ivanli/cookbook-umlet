51b1d8ae4219c0a228511f8d3cb9539a56ff4865
Ivan Li <ivankyli@gmail.com>
2015-07-08 15:05:19 +1200

    Fixing recipe with incorrect version

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,7 +10,7 @@
 include_recipe 'windows'
 
 windows_zipfile node['umlet']['out_path'] do
-  source "http://www.umlet.com/umlet_#{node['umlet']['version'].gsub('.','_')}/umlet_#{node['umlet']['out_path']}.zip"
+  source "http://www.umlet.com/umlet_#{node['umlet']['version'].gsub('.','_')}/umlet_#{node['umlet']['version']}.zip"
   action :unzip
   not_if {::Dir.exists?(node['umlet']['out_path'])}
 end
